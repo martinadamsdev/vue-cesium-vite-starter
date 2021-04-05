9a2c0ddf64b02529860c614d877543627a93e852
wangliquan <w3cfed@gmail.com>
2021-04-06 04:04:33 +0800

    deploy

--- src/components/VViewer.vue
+++ src/components/VViewer.vue
@@ -97,7 +97,7 @@ export default {
 
       return await viewer.scene.primitives.add(
         Model.fromGltf({
-          url: "vue-cesium-vite-app/model/man.glb",
+          url: "model/man.glb",
           modelMatrix,
           // minimumPixelSize: 0.0,
         })
