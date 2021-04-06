b66517157c9629f8c91693060a59bec42c358469
wangliquan <w3cfed@gmail.com>
2021-04-07 00:02:57 +0800

    feat(Github) add github link

--- src/components/VViewer.vue
+++ src/components/VViewer.vue
@@ -6,6 +6,34 @@
     <div @click="freeRoaming">自由漫游</div>
     <!-- <div @click="pathRoaming()">路径漫游</div> -->
     <div @click="resetCamera">解除锁定</div>
+    <div @click="resetCamera">
+      <a
+        aria-label="martinageradams"
+        title="GitHub"
+        href="https://github.com/martinageradams/vue-cesium-vite-app"
+      >
+        <svg
+          height="20"
+          version="1.1"
+          width="35"
+          aria-hidden="true"
+        >
+          <text x="0" y="16" fill="black">Star</text>
+        </svg>
+        <svg
+          height="20"
+          viewBox="0 0 16 16"
+          version="1.1"
+          width="20"
+          aria-hidden="true"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+          ></path>
+        </svg>
+      </a>
+    </div>
   </div>
 </template>
 
@@ -37,7 +65,7 @@ import {
   changeDefaultCamera,
   freeRoaming,
   firstPersonCamera,
-  thirdPersonCamera
+  thirdPersonCamera,
 } from "../utils/roam";
 var center;
 export default {
@@ -46,7 +74,7 @@ export default {
       window.viewer = init3d();
       // 开发测试工具
       // 发版需要注释掉
-      viewer.extend(viewerCesiumInspectorMixin);
+      // viewer.extend(viewerCesiumInspectorMixin);
       viewer._cesiumWidget._creditContainer.style.display = "none";
       window.model = await initRoamingModel(viewer, {
         position: new Cartesian3.fromDegrees(110, 40, 0),
@@ -91,19 +119,19 @@ export default {
 
     const firstPerson = () => {
       firstPersonCamera(viewer, model);
-    }
+    };
     const thirdPerson = () => {
       thirdPersonCamera(viewer, model);
-    }
+    };
 
     const resetCamera = () => {
-      changeDefaultCamera(viewer, true)
-    }
+      changeDefaultCamera(viewer, true);
+    };
 
     return {
       firstPerson,
       thirdPerson,
-      resetCamera
+      resetCamera,
     };
   },
 };
